68843164736b0c3fb0352de2714d63bbeb99abe4
nameyouxuan <nameyouxuan@gmail.com>
2023-10-02 21:37:15 +0800

    fix

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -6,6 +6,9 @@ return {
     },
     config = function()
         require("mason").setup({
+            automatic_installation = true,
+            -- 自动安装 Language Servers
+            -- ensure_installed = {},
             ui = {
                 icons = {
                     package_installed = "✓",
@@ -13,31 +16,39 @@ return {
                     package_uninstalled = "✗",
                 },
             },
+
             max_concurrent_installers = 10,
         })
 
+        local lspconfig = require('lspconfig')
+
+        local servers = {
+            "clangd",
+            "cmake",
+            "lua_ls",
+            "tsserver",
+            "tailwindcss",
+            "bashls",
+            "cssls",
+            "dockerls",
+            "emmet_ls",
+            "html",
+            "jsonls",
+            "pyright",
+            "rust_analyzer",
+            "taplo",
+            "yamlls",
+            "gopls",
+        }
+
         require("mason-lspconfig").setup({
-            -- 自动安装 Language Servers
-            automatic_installation = true,
             -- 确保安装，根据需要填写
-            ensure_installed = {
-                "clangd",
-                "cmake",
-                "lua_ls",
-                "tsserver",
-                "tailwindcss",
-                "bashls",
-                "cssls",
-                "dockerls",
-                "emmet_ls",
-                "html",
-                "jsonls",
-                "pyright",
-                "rust_analyzer",
-                "taplo",
-                "yamlls",
-                "gopls",
-            },
+            ensure_installed = servers,
         })
+        for _, server in ipairs(servers) do
+            lspconfig[server].setup {}
+        end
+        -- require("lspconfig").rust_analyzer.setup {}
+        -- require("lspconfig").clangd.setup {}
     end,
 }
